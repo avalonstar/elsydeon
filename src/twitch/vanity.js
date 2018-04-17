f230edd74df30895c112e4ad4f2dabfebd9f81f6
Bryan Veloso <bryan@avalonstar.com>
2018-04-16 23:04:17 -0700

    Arrow'd!

--- src/twitch/vanity.js
+++ src/twitch/vanity.js
@@ -1,24 +1,24 @@
 'use strict';
 
 const heybryan = (client, { channel }) =>
-  client.action(channel, ` / SHIELD OATH DAMNIT. avalonDERP`);
+  client.action(channel, ` ⤇ WOAH BUCKO. avalonBAKA`);
 
 const heyeasy = (client, { channel }) =>
-  client.action(channel, ` / Hey, I got a Thundercloud proc. avalonDERP`);
+  client.action(channel, ` ⤇ Hey, I got a Thundercloud proc. avalonDERP`);
 
 const heyfires = (client, { channel }) =>
-  client.action(channel, ` / SHIELD OATH DAMNIT. avalonBAKA`);
+  client.action(channel, ` ⤇ SHIELD OATH DAMNIT. avalonBAKA`);
 
 const heyheather = (client, { channel }) =>
-  client.action(channel, ` / #BlameEos`);
+  client.action(channel, ` ⤇ #BlameEos`);
 
 const heykay = (client, { channel }) =>
-  client.action(channel, ` / REMEMBER TO SAAAAAVVVEEEEE~ avalonBLIND`);
+  client.action(channel, ` ⤇ REMEMBER TO SAAAAAVVVEEEEE~ avalonBLIND`);
 
 const provoke = (client, { channel }) =>
   client.action(
     channel,
-    ` / "BRYAN YOU PROVOKED IT NOOOOOOOOOOO" ~ @Dmal_ , 2017`
+    ` ⤇ "BRYAN YOU PROVOKED IT NOOOOOOOOOOO" ~ @Dmal_ , 2017`
   );
 
 module.exports = {
