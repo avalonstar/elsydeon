7e27c01f8b818ba49f2b421b9e2d49620a4b05b2
Bryan Veloso <bryan@avalonstar.com>
2018-06-11 17:17:59 -0700

    WRONG.

--- src/twitch/index.js
+++ src/twitch/index.js
@@ -32,7 +32,7 @@ const getCommand = (command, client, input, args) => {
     andback: () => commands.andback(client, args),
     forward: () => commands.forward(client, args),
     fuckedd: () => commands.fuckedd(client, args),
-    hype: () => commands.subhype(client, args),
+    hype: () => commands.hype(client, args),
     punt: () => commands.punt(client, args),
     slap: () => commands.slap(client, input, args),
     subhype: () => commands.subhype(client, args),
