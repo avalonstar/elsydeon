ce11c6b42fce36f8b20d0c74c34c27f4c0eadd05
Bryan Veloso <bryan@avalonstar.com>
2019-06-12 11:26:41 -0700

    HOLY.

--- src/twitch/commands/heybryan.js
+++ src/twitch/commands/heybryan.js
@@ -2,6 +2,6 @@ export default {
   name: 'heybryan',
   aliases: [],
   async execute(client, { channel }) {
-    client.say(channel, `/me ⤇ THIS HAND OF MINE GLOWS WITH AN AWESOME POWER. IT'S BURNING GRIP TELLS ME TO DEFEAT YOU. TAKE THIS. MY LOVE. MY ANGER. AND ALL OF MY SORROW. SHININGGGGGGGG FINGGEERRRR!~ avalonRAGE`);
+    client.say(channel, `/me ⤇ HOLY...`);
   }
 }
